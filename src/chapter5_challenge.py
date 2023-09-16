61d54d5868b55107c9d43ae125da0023a0d74476
thomashk2001 <thomashk2001@outlook.com>
2023-09-16 08:38:28 -0600

    Deleted unnecessary function called on challenge

--- src/chapter5_challenge.py
+++ src/chapter5_challenge.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 ray_origin = Point(0, 0, -5)
 wall_z = 10
 wall_size = 7
-canvas_pixel = 1000
+canvas_pixel = 100
 pixel_size = wall_size / canvas_pixel
 half = wall_size / 2
 canvas = Canvas(canvas_pixel, canvas_pixel)
@@ -24,7 +24,6 @@ for y in range(canvas_pixel):
     world_x = -half + pixel_size * x
     position = Point(world_x, world_y, wall_z)
     direction = position - ray_origin
-    direction.normalize()
     r = Ray(ray_origin, direction.normalize())
     xs = intersect(shape, r)
     if len(xs) > 0:
